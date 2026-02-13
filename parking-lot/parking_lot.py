7cbe67d535870fb0c87c888103c7ac3546ef2c96
Eric Zheng <erryzheng@gmail.com>
2026-02-12 23:40:58 -0500

    parking lot

---
+++ parking-lot/parking_lot.py
@@ -0,0 +1,23 @@
+class ParkingLot:
+    pass
+
+class Level:
+    pass
+
+class SpotType:
+    pass
+
+class Vehicle:
+    pass
+
+class Motorcycle(Vehicle):
+    pass
+
+class Car(Vehicle):
+    pass
+
+class Bus(Vehicle):
+    pass
+
+class Payment:
+    pass
